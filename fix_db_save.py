ab2a466db0efbf802e9d99eccbfe6a9345a25628
shahzebirfan-coder <shahzeb.irfan@gmail.com>
2026-06-18 14:19:36 +0500

    Delete fix_db_save.py

--- fix_db_save.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('assets/js/db.js', 'r') as f:
-    content = f.read()
-
-# We need to catch QuotaExceededError inside the DB.save hook inside cloudsync.js, OR we need to make sure DB.save catches it without throwing so execution can continue.
-# Currently, DB.save in db.js catches it and calls alert, but DOES NOT THROW! That's good.
-# Let's check cloudsync.js hook.
-
