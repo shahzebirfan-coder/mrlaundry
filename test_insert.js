19e556dd687a6c4b98485a35ad1c0da9ffc40296
shahzebirfan-coder <shahzeb.irfan@gmail.com>
2026-06-18 14:27:54 +0500

    Delete test_insert.js

--- test_insert.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const js = fs.readFileSync('/tmp/test-mrlaundry/assets/js/pages/pos.js', 'utf8');
-try { new Function(js); console.log('Syntax OK orders'); } catch(e) { console.error('Syntax Error orders:', e.message); }
-
