328ad37808864f2b9f80c8274bb8d44714359090
shahzebirfan-coder <shahzeb.irfan@gmail.com>
2026-06-18 14:27:34 +0500

    Delete test_db_size.js

--- test_db_size.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const dbText = fs.readFileSync('assets/js/db.js', 'utf8');
-
-// I am pretty sure the localstorage quota is the main problem. 
-// A single localStorage item can hold max ~5MB. If it hits that, save() throws QuotaExceededError.
