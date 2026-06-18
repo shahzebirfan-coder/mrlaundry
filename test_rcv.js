cd9855812da1e91d237d3fb5d01060e7399f4ec9
shahzebirfan-coder <shahzeb.irfan@gmail.com>
2026-06-18 14:28:54 +0500

    Delete test_rcv.js

--- test_rcv.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const js = fs.readFileSync('assets/js/pages/orders.js', 'utf8');
-try { new Function(js); console.log('Syntax OK orders'); } catch(e) { console.error('Syntax Error orders:', e.message); }
-
