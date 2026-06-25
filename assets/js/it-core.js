7c33d86060c42023448684c61b4a51b1a198470f
shahzebirfan-coder <shahzeb.irfan@gmail.com>
2026-06-25 13:36:17 +0500

    Delete assets directory

--- assets/js/it-core.js
+++
@@ -1,8 +0,0 @@
-// CelineSoft Encrypted IT Core
-window._IT_BRAND = {
-    n: atob('Q2VsaW5lU29mdA=='),
-    t: atob('RW1wb3dlcmluZyBCdXNpbmVzc2VzIHdpdGggU21hcnQgQ29kZQ=='),
-    p: atob('KzkyIDMwMiA4MjQ0ODAz'),
-    e: atob('c2hhaHplYi5pcmZhbkBnbWFpbC5jb20='),
-    pwd: atob('SVRBZG1pbkAxMjM=')
-};
